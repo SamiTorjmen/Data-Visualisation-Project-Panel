79be5df36865acc5a2d1025057453f8ff037102b
SamiTorjmen <sami.torjmen@gmail.com>
2023-04-09 23:54:41 +0200

    add dashboard module to docs

---
+++ docs/dashboard/__init__.py
@@ -0,0 +1,11 @@
+from dashboard.plots import table_plotly
+from dashboard.plots import pie_quali 
+from dashboard.plots import histogram_quali 
+from dashboard.plots import boxplot_quali_quanti 
+from dashboard.plots import scatter_quanti_quanti
+from dashboard.plots import plotting_target_feature
+
+
+from dashboard.helpers import plotly_to_plt_colors
+from dashboard.helpers import color_s
+from dashboard.helpers import categarray
